4d5df6c78920f811ca4fe16143e1ec326db69cae
bradallgood <brad.allgood.personal@gmail.com>
2023-09-12 16:23:12 -0500

    fixed non network

--- dtv_nfl.py
+++ dtv_nfl.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 odds_tables = pd.read_html('./dtv.html')
 
 #print(len(odds_tables))
-#print(odds_tables[0])
+print(odds_tables[0])
 
 table = odds_tables[0]
 
@@ -20,18 +20,25 @@ for index, row in table.iterrows():
         if prev_time != row['Time']:
             print('\n')
         count = row['Channels'].split(' ')
-        if len(count) == 4 or len(count) == 6:
+        if len(count) == 4 :
              #print(f'{row["Date"]:15}{row["Event"]:15}{row["Time"]:15}{count[0]:15}')
              print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[0])) 
         elif len(count) == 9 or len(count)==14:
-             #print(f'{row["Date"]:15}{row["Event"]:15}{row["Time"]:15}{count[3]:15}')
-             if count[3] == 'PRIMETNF':
-                print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[0]))
-             else: 
-                print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[3]))
-        elif len(count) == 6:
-             #print(f'{row["Date"]:15}{row["Event"]:15}{row["Time"]:15}{count[0]:15}')
-             print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[0])) 
+             if len(count) == 9:
+                 if count[3] in ['FOXe','CBSe']:
+                    print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[5]))
+                 else:
+                     print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[3]))
+             else:
+                 #print(f'{row["Date"]:15}{row["Event"]:15}{row["Time"]:15}{count[3]:15}')
+                 if count[3] == 'PRIMETNF':
+                    print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[0]))
+                 else: 
+                    print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[3]))
+        #elif len(count) == 6:
+        #     print("in sixer")
+        #     #print(f'{row["Date"]:15}{row["Event"]:15}{row["Time"]:15}{count[0]:15}')
+        #     print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[0])) 
     prev_time = row['Time']
 
 
