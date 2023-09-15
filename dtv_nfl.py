130d2d6700c61b5b31707f23fb1a314d7a2bf3c3
bradallgood <brad.allgood.personal@gmail.com>
2023-09-15 10:30:05 -0500

    init

--- dtv_nfl.py
+++ dtv_nfl.py
@@ -5,11 +5,10 @@ from unicodedata import normalize
 import html5lib 
 from bs4 import BeautifulSoup
 
-
-odds_tables = pd.read_html('./dtv.html')
+odds_tables = pd.read_html('./dtv_week_2.html')
 
 #print(len(odds_tables))
-print(odds_tables[0])
+#print(odds_tables[0])
 
 table = odds_tables[0]
 
@@ -18,7 +17,7 @@ prev_time = ''
 for index, row in table.iterrows():
     if row['Date'] !=  row['Event']:
         if prev_time != row['Time']:
-            print('\n')
+            print('')
         count = row['Channels'].split(' ')
         if len(count) == 4 :
              #print(f'{row["Date"]:15}{row["Event"]:15}{row["Time"]:15}{count[0]:15}')
@@ -28,18 +27,14 @@ for index, row in table.iterrows():
                  if count[3] in ['FOXe','CBSe']:
                     print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[5]))
                  else:
-                     print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[3]))
+                    if count[3] == 'PRIMETNF':
+                       print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[0]))
+                    else:
+                       print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[3]))
              else:
                  #print(f'{row["Date"]:15}{row["Event"]:15}{row["Time"]:15}{count[3]:15}')
-                 if count[3] == 'PRIMETNF':
-                    print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[0]))
-                 else: 
-                    print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[3]))
-        #elif len(count) == 6:
-        #     print("in sixer")
-        #     #print(f'{row["Date"]:15}{row["Event"]:15}{row["Time"]:15}{count[0]:15}')
-        #     print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[0])) 
-    prev_time = row['Time']
+                print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[3]))
+        prev_time = row['Time']
 
 
 
