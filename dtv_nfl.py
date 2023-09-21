bd289732fe3574730e5adf4ec16f59073bc1457b
bradallgood <brad.allgood.personal@gmail.com>
2023-09-21 10:43:10 -0500

    debug

--- dtv_nfl.py
+++ dtv_nfl.py
@@ -5,7 +5,39 @@ from unicodedata import normalize
 import html5lib 
 from bs4 import BeautifulSoup
 
-odds_tables = pd.read_html('./dtv_week_2.html')
+soup = BeautifulSoup(open('./dtv_week_3_mod.html'), 'html.parser')
+prev_date_time = ''
+results = soup.find_all("div", class_="row d-print-none mt-3 no-gutters")
+for result in results:
+    #print('-----------------------------------------------')
+    if result.find("h2", "group-header-text"):
+        date_read = result.find("h2", "group-header-text")
+    time_read = result.find("span", {'data-v-42822f8f': True})
+    teams = result.find_all('a', {'data-v-2306d078': True})
+    home = teams[1].text.strip()
+    away = teams[3].text.strip()
+    i = 0 
+    ch_string = ''
+    channels = result.find_all('span', class_="v-chip__content")
+    while i < len(channels):
+        if len(channels[i].text.split()) == 3:
+            channel,sep,name = channels[i].text.split()
+            ch_string = ch_string + f' {name} {channel}  '
+        else:
+            ch_string = ch_string + f'Name: {name}  '
+        i +=1
+    date_time_str = date_read.text.strip() + ' ' + time_read.text.strip()
+
+    if date_time_str == prev_date_time:
+        print('%10s %10s %15s %15s %60s' % (date_read.text.strip(),time_read.text.strip(),away,home,ch_string))
+    else:
+        print('\n%10s %10s %15s %15s %60s' % (date_read.text.strip(),time_read.text.strip(),away,home,ch_string))
+    prev_date_time = date_time_str
+
+
+
+'''
+odds_tables = pd.read_html('./dtv_week_3.html')
 
 #print(len(odds_tables))
 #print(odds_tables[0])
@@ -35,7 +67,7 @@ for index, row in table.iterrows():
                  #print(f'{row["Date"]:15}{row["Event"]:15}{row["Time"]:15}{count[3]:15}')
                 print('%s %30s %20s %20s' % (row['Date'],row['Event'],row['Time'],count[3]))
         prev_time = row['Time']
-
+'''
 
 
 
